ddbbf9b30a2168db6d917dbfae51aa4542216218
LampSeeker <rampseeker@gmail.com>
2024-03-15 02:54:11 +0900

    mainactivity-toolbar

--- app/src/main/java/com/portfolio/ardronekotlin/MainActivity.kt
+++ app/src/main/java/com/portfolio/ardronekotlin/MainActivity.kt
@@ -2,13 +2,35 @@ package com.portfolio.ardronekotlin
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-
+import com.google.android.material.appbar.MaterialToolbar
+import com.portfolio.ardronekotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(){
+
+    lateinit var topAppbar: MaterialToolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //툴바 설정
+        menubind = binding
+        topAppbar = fin
+
+
+
+
+}
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            else -> {}
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
